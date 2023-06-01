c5dad6aca48673606e1b745cafec1b07eb276174
Angel <angel@tw>
2023-05-31 22:41:14 -0700

    ran prettier formatter

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,18 @@
-import React from "react"
-import { MdKeyboardReturn } from "react-icons/md"
+import React from "react";
+import { MdKeyboardReturn } from "react-icons/md";
 
 // Unused props for now but all components should have this, in case they are needed in the future.
-type ButtonProps = {
+type ButtonProps = {};
 
+const Button: React.FC<ButtonProps> = () => {
+  return (
+    <div className="flex flex-wrap items-center justify-center gap-2 p-5">
+      <button className="flex cursor-pointer items-center justify-between rounded bg-brand-gray_light px-3 py-1.5 text-brand-red hover:bg-brand-gray focus:outline-none">
+        <span className="px-1">Submit</span>
+        <MdKeyboardReturn />
+      </button>
+    </div>
+  );
 };
-
-const Button:React.FC<ButtonProps> = () => {
-    return <div className="p-5 flex flex-wrap justify-center items-center gap-2">
-                <button className='flex items-center justify-between focus:outline-none bg-brand-gray_light hover:bg-brand-gray py-1.5 px-3 cursor-pointer rounded text-brand-red'>
-                    <span className="px-1">Submit</span>
-                    <MdKeyboardReturn />
-                </button>
-            </div>
-}
 
 export default Button;
