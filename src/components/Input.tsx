4b6bce3d7fc46a4f6b7cb7e6dac1db6510a3602b
alex <atol514@gmail.com>
2023-06-03 16:39:09 -0700

    squash commit:  Package.json : created shorthand for seed command.  tsconfig.json : made ts-node use a specific compiler so that the seed                  script would work.   Created Input component and debued it on index.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,41 @@
-export default function Input() {
-    
+export default function Input({
+  type,
+  id,
+  name,
+  ghostText,
+  cssString,
+}: {
+  type: string;
+  id: string;
+  name?: string;
+  ghostText?: string;
+  cssString?: string;
+}) {
+  var inputField;
+
+  switch (type) {
+    case "textarea":
+      inputField = (
+        <textarea
+          id={id}
+          name={name}
+          placeholder={ghostText}
+          className={cssString}
+        ></textarea>
+      );
+      break;
+    default:
+      inputField = (
+        <input
+          type="text"
+          id={id}
+          name={name}
+          placeholder={ghostText}
+          className={cssString}
+        />
+      );
+      break;
+  }
+
+  return <>{inputField}</>;
 }
