ab32871f172fa7da95261469c2dd6f2d247f2f8c
meow <meow@taiwan>
2023-06-02 18:24:31 -0700

    update

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,3 +1,23 @@
-export default function TopMenu() {
-    
+import { PropsWithChildren } from "react"
+
+type ItemProps = PropsWithChildren<{
+   onClick?: () => void
+   className?: string
+}>
+
+type MenuProps = PropsWithChildren<{
+   className?: string
+
+}>
+
+export function TopMenu({ children, ...props }: MenuProps) {
+   return (
+      <menu className="">
+         <div {...props}>{children}</div>
+      </menu>
+   )
+}
+
+export function Item({ children, ...props }: ItemProps) {
+   return <div {...props}>{children}</div>
 }
