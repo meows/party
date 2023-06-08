823b381225c3e19bed3ce7561d670bec723ca56c
meows <meows@users.noreply.github.com>
2023-06-07 18:21:45 -0700

    TopMenu: added component and modified root page (#13)

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+type MenuChoice = "events"
+    | "search"
+    | "message"
+    | "accounts"
--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,3 +1,35 @@
-export default function TopMenu() {
-    
+import { PropsWithChildren } from "react"
+
+type ItemProps = PropsWithChildren<{
+   onClick?: () => void
+   className?: string
+}>
+
+type MenuProps = PropsWithChildren<{
+   className?: string
+   active: MenuChoice
+}>
+
+export default function Menu({ active, children, ...props }: MenuProps) {
+   return (
+      <menu id="TopMenu" {...props}>
+         <li id="logo">XYZ</li>
+         <li title="Events" className={active === "events" ? "active" : ""}>Events</li>
+         <li title="Search" className={active === "search" ? "active" : ""}>Search</li>
+         <li title="Message" className={active === "message" ? "active" : ""}>Message</li>
+         <li title="Accounts" className={active === "accounts" ? "active" : ""}>Accounts</li>
+      </menu>
+   )
+}
+
+export function TopMenu({ children, ...props }: MenuProps) {
+   return (
+      <menu>
+         <div {...props}>{children}</div>
+      </menu>
+   )
+}
+
+export function Item({ children, ...props }: ItemProps) {
+   return <div {...props}>{children}</div>
 }
