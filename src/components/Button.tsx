016067903a638ea9ed785268b20928b9c967c78f
Angel <angel@tw>
2023-06-10 23:44:16 -0700

    added some buttons and dropdown to homepage, currently very ugly in bad condition

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,10 +3,10 @@ import { MdKeyboardReturn } from "react-icons/md";
 
 type ButtonProps = React.PropsWithChildren;
 
-function Button({ children }: ButtonProps) {
+const Button: React.FC<ButtonProps> = ({ children }) => {
   return (
     <div className="flex flex-wrap items-center justify-center gap-2 p-5">
-      <button className="flex cursor-pointer items-center justify-between rounded bg-brand-red px-3 py-1.5 text-brand-gray_light hover:bg-brand-red_dark focus:outline-none">
+      <button className="flex cursor-pointer items-center justify-between rounded bg-brand-red px-3 py-1.5 text-brand-gray_light active:bg-brand-red_dark hover:bg-brand-red_dark focus:outline-none">
         <span className="px-1">{children}</span>
         <MdKeyboardReturn />
       </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,123 @@
 import { type NextPage } from "next";
+import React, { ReactNode, useState } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
 import Button from "~/components/Button";
 import Input from "~/components/Input";
 import { api } from "~/utils/api";
-import Menu from "~/components/TopMenu";
+import TopMenu from "~/components/TopMenu";
+import Select, { createFilter, StylesConfig } from "react-select";
+import { defaultTheme } from "react-select";
+
+export interface StateOption {
+  readonly value: string;
+  readonly label: string;
+}
+
+export interface ColourOption {
+  readonly value: string;
+  readonly label: string;
+  readonly color: string;
+  readonly isFixed?: boolean;
+  readonly isDisabled?: boolean;
+}
+
+
+export const stateOptions: readonly StateOption[] = [
+  { value: "AL", label: "Alabama" },
+  { value: "AK", label: "Alaska" },
+  { value: "AS", label: "American Samoa" },
+  { value: "AZ", label: "Arizona" },
+  { value: "AR", label: "Arkansas" },
+  { value: "CA", label: "California" },
+  { value: "CO", label: "Colorado" },
+  { value: "CT", label: "Connecticut" },
+  { value: "DE", label: "Delaware" },
+  { value: "DC", label: "District Of Columbia" },
+  { value: "FM", label: "Federated States Of Micronesia" },
+  { value: "FL", label: "Florida" },
+  { value: "GA", label: "Georgia" },
+  { value: "GU", label: "Guam" },
+  { value: "HI", label: "Hawaii" },
+  { value: "ID", label: "Idaho" },
+  { value: "IL", label: "Illinois" },
+  { value: "IN", label: "Indiana" },
+  { value: "IA", label: "Iowa" },
+  { value: "KS", label: "Kansas" },
+  { value: "KY", label: "Kentucky" },
+  { value: "LA", label: "Louisiana" },
+  { value: "ME", label: "Maine" },
+  { value: "MH", label: "Marshall Islands" },
+  { value: "MD", label: "Maryland" },
+  { value: "MA", label: "Massachusetts" },
+  { value: "MI", label: "Michigan" },
+  { value: "MN", label: "Minnesota" },
+  { value: "MS", label: "Mississippi" },
+  { value: "MO", label: "Missouri" },
+  { value: "MT", label: "Montana" },
+  { value: "NE", label: "Nebraska" },
+  { value: "NV", label: "Nevada" },
+  { value: "NH", label: "New Hampshire" },
+  { value: "NJ", label: "New Jersey" },
+  { value: "NM", label: "New Mexico" },
+  { value: "NY", label: "New York" },
+  { value: "NC", label: "North Carolina" },
+  { value: "ND", label: "North Dakota" },
+  { value: "MP", label: "Northern Mariana Islands" },
+  { value: "OH", label: "Ohio" },
+  { value: "OK", label: "Oklahoma" },
+  { value: "OR", label: "Oregon" },
+  { value: "PW", label: "Palau" },
+  { value: "PA", label: "Pennsylvania" },
+  { value: "PR", label: "Puerto Rico" },
+  { value: "RI", label: "Rhode Island" },
+  { value: "SC", label: "South Carolina" },
+  { value: "SD", label: "South Dakota" },
+  { value: "TN", label: "Tennessee" },
+  { value: "TX", label: "Texas" },
+  { value: "UT", label: "Utah" },
+  { value: "VT", label: "Vermont" },
+  { value: "VI", label: "Virgin Islands" },
+  { value: "VA", label: "Virginia" },
+  { value: "WA", label: "Washington" },
+  { value: "WV", label: "West Virginia" },
+  { value: "WI", label: "Wisconsin" },
+  { value: "WY", label: "Wyoming" },
+];
+export const colourOptions: readonly ColourOption[] = [
+  { value: "ocean", label: "Ocean", color: "#00B8D9", isFixed: true },
+  { value: "blue", label: "Blue", color: "#0052CC", isDisabled: true },
+  { value: "purple", label: "Purple", color: "#5243AA" },
+  { value: "red", label: "Red", color: "#FF5630", isFixed: true },
+  { value: "orange", label: "Orange", color: "#FF8B00" },
+  { value: "yellow", label: "Yellow", color: "#FFC400" },
+  { value: "green", label: "Green", color: "#36B37E" },
+  { value: "forest", label: "Forest", color: "#00875A" },
+  { value: "slate", label: "Slate", color: "#253858" },
+  { value: "silver", label: "Silver", color: "#666666" },
+];
+
+const Checkbox = (props: JSX.IntrinsicElements["input"]) => (
+  <input type="checkbox" {...props} />
+);
+
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
+  const [ignoreCase, setIgnoreCase] = useState(false);
+  const [ignoreAccents, setIgnoreAccents] = useState(false);
+  const [trim, setTrim] = useState(false);
+  const [matchFromStart, setMatchFromStart] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [value, setValue] = useState<StateOption | null>();
+
+  const filterConfig = {
+    ignoreCase,
+    ignoreAccents,
+    trim,
+    matchFrom: matchFromStart ? ("start" as const) : ("any" as const),
+  };
   return (
     <>
       <Head>
@@ -19,7 +128,94 @@ const Home: NextPage = () => {
 
       <main className="overflow-y-auto bg-gradient-to-b from-gray-900 to-gray-700">
         <div className="m-auto flex min-h-screen max-w-[600px] flex-col bg-gradient-to-b from-gray-900 to-gray-700 px-5">
-          <Menu active="accounts" />
+          <TopMenu active="accounts" />
+          <div className="flex min-w-[375px] flex-row items-center justify-center gap-2 p-5">
+            <Select
+              className="w-1/2 min-w-[150px]"
+              defaultValue={colourOptions[0]}
+              isClearable
+              isSearchable
+              name="color"
+              options={colourOptions}
+              filterOption={createFilter(filterConfig)}
+            />
+            <Select
+              className="w-1/2 min-w-[150px]"
+              defaultValue={colourOptions[0]}
+              isClearable
+              isSearchable
+              name="color"
+              options={colourOptions}
+              filterOption={createFilter(filterConfig)}
+            />
+          </div>
+          <div className="flex min-w-[375px] flex-row items-center justify-center gap-2 p-5">
+            <Dropdown
+              isOpen={isOpen}
+              onClose={() => setIsOpen(false)}
+              target={
+                <button
+                  // iconAfter={<ChevronDown />}
+                  onClick={() => setIsOpen((prev) => !prev)}
+                  className={`active=${isOpen}`}
+                >
+                  {value ? `State: ${value.label}` : "Select a TimeFram"}
+                </button>
+              }
+            >
+              <Select
+                autoFocus
+                backspaceRemovesValue={false}
+                components={{ DropdownIndicator, IndicatorSeparator: null }}
+                controlShouldRenderValue={false}
+                hideSelectedOptions={false}
+                isClearable={false}
+                menuIsOpen
+                onChange={(newValue) => {
+                  setValue(newValue);
+                  setIsOpen(false);
+                }}
+                options={stateOptions}
+                placeholder="Search..."
+                styles={selectStyles}
+                tabSelectsValue={false}
+                value={value}
+              />
+            </Dropdown>
+            <Dropdown
+              isOpen={isOpen}
+              onClose={() => setIsOpen(false)}
+              target={
+                <button
+                  // iconAfter={<ChevronDown />}
+                  onClick={() => setIsOpen((prev) => !prev)}
+                  className={`active=${isOpen}`}
+                >
+                  {value ? `City: ${value.label}` : "Select a City"}
+                </button>
+              }
+            >
+              <Select
+                autoFocus
+                backspaceRemovesValue={false}
+                components={{ DropdownIndicator, IndicatorSeparator: null }}
+                controlShouldRenderValue={false}
+                hideSelectedOptions={false}
+                isClearable={false}
+                menuIsOpen
+                onChange={(newValue) => {
+                  setValue(newValue);
+                  setIsOpen(false);
+                }}
+                options={stateOptions}
+                placeholder="Search..."
+                styles={selectStyles}
+                tabSelectsValue={false}
+                value={value}
+              />
+            </Dropdown>
+          </div>
+
           <p className="px-5 py-5 text-2xl text-white">
             {hello.data ? hello.data.greeting : "Loading tRPC query..."}
           </p>
@@ -57,3 +253,77 @@ const AuthShowcase: React.FC = () => {
     </div>
   );
 };
+
+const { colors } = defaultTheme;
+
+const selectStyles: StylesConfig<StateOption, false> = {
+  control: (provided) => ({
+    ...provided,
+    minWidth: 240,
+    // minWidth: 100,
+    margin: 8,
+  }),
+  menu: () => ({ boxShadow: "inset 0 1px 0 rgba(0, 0, 0, 0.1)" }),
+};
+
+// styled components
+
+const Menu = (props: JSX.IntrinsicElements["div"]) => {
+  const shadow = "hsla(218, 50%, 10%, 0.1)";
+  return (
+    <div
+      className="rounded-4 z-2 absolute mt-8 bg-white shadow-md"
+      {...props}
+    />
+  );
+};
+const Blanket = (props: JSX.IntrinsicElements["div"]) => (
+  <div className="z-1 fixed inset-0" {...props} />
+);
+const Dropdown = ({
+  children,
+  isOpen,
+  target,
+  onClose,
+}: {
+  children?: ReactNode;
+  readonly isOpen: boolean;
+  readonly target: ReactNode;
+  readonly onClose: () => void;
+}) => (
+  <div className="relative bg-brand-gray_light rounded p-3">
+    {target}
+    {isOpen ? <Menu>{children}</Menu> : null}
+    {isOpen ? <Blanket onClick={onClose} /> : null}
+  </div>
+);
+const Svg = (p: JSX.IntrinsicElements["svg"]) => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    focusable="false"
+    role="presentation"
+    {...p}
+  />
+);
+const DropdownIndicator = () => (
+  <div className="text-neutral20 h-24 w-32">
+    <Svg>
+      <path
+        d="M16.436 15.085l3.94 4.01a1 1 0 0 1-1.425 1.402l-3.938-4.006a7.5 7.5 0 1 1 1.423-1.406zM10.5 16a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </Svg>
+  </div>
+);
+const ChevronDown = () => (
+  <Svg style={{ marginRight: -6 }}>
+    <path
+      d="M8.292 10.293a1.009 1.009 0 0 0 0 1.419l2.939 2.965c.218.215.5.322.779.322s.556-.107.769-.322l2.93-2.955a1.01 1.01 0 0 0 0-1.419.987.987 0 0 0-1.406 0l-2.298 2.317-2.307-2.327a.99.99 0 0 0-1.406 0z"
+      fill="currentColor"
+      fillRule="evenodd"
+    />
+  </Svg>
+);
