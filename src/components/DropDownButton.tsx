a62e825ee4caf27dcd389c62149106d9927873a0
meow <meow@taiwan>
2023-06-25 18:27:25 -0700

    update

--- src/components/DropDownButton.tsx
+++ src/components/DropDownButton.tsx
@@ -1,7 +1,6 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useState } from "react";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { atom, useAtom } from "jotai";
 
 // —————————————————————————————————————————————————————————————————————————————
 // Environment
@@ -17,31 +16,18 @@ type DropDownButtonProps<T> = React.PropsWithChildren<{
   defaultValue: T;
 }>;
 
-// type WithInitialValue<Value> = {
-//   init: Value;
-// };
-
 type DropDownItemProps = React.PropsWithChildren<{
-  // onClick?: React.MouseEventHandler<HTMLButtonElement>;
-  // useSelectedAtom?: () => [string, (value: string) => void];
-  // useSelectedAtom?: PrimitiveAtom<T> & WithInitialValue<T>;
+  setSelected: (value: string) => void;
 }>;
-
-
-// TODO: Pass down useSelectedAtom to the children instead of being global.
-//       so that each instance of DropDownButton has it's own atom.
-// Generate a unique atom for each instance of DropDownButton
-// function createSelectedAtom(defaultValue: any) {
-//   return atom<typeof defaultValue>(defaultValue);
-// }
-
-const useSelectedAtom = atom("");
 
 // —————————————————————————————————————————————————————————————————————————————
 // Component
 
-const DropDownItem: React.FC<DropDownItemProps> = ({ children, ...props }) => {
-  const [, setSelected] = useAtom(useSelectedAtom);
+const DropDownItem: React.FC<DropDownItemProps> = ({
+  children,
+  setSelected,
+  ...props
+}) => {
   return (
     <Menu.Item {...props}>
       {({ active }) => (
@@ -51,7 +37,7 @@ const DropDownItem: React.FC<DropDownItemProps> = ({ children, ...props }) => {
           }}
           className={classNames(
             active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-            "block w-full px-4 py-2 text-left text-sm whitespace-nowrap"
+            "block w-full whitespace-nowrap px-4 py-2 text-left text-sm"
           )}
         >
           {children}
@@ -59,20 +45,12 @@ const DropDownItem: React.FC<DropDownItemProps> = ({ children, ...props }) => {
       )}
     </Menu.Item>
   );
-}
+};
 
-const DropDownButton: React.FC<DropDownButtonProps<SortEvents | SortDateRange>> = ({
-  children,
-  defaultValue,
-  name,
-}) => {
-  // const useSelectedAtom = createSelectedAtom(defaultValue);
-  const [selected, setSelected] = useAtom(useSelectedAtom);
-
-  useEffect(() => {
-    // I don't like that this gets passed as string.
-    setSelected(defaultValue as string);
-  }, []);
+const DropDownButton: React.FC<
+  DropDownButtonProps<SortEvents | SortDateRange>
+> = ({ children, defaultValue, name }) => {
+  const [selected, setSelected] = useState(defaultValue);
 
   return (
     <Menu
@@ -80,7 +58,7 @@ const DropDownButton: React.FC<DropDownButtonProps<SortEvents | SortDateRange>> 
       className="relative inline-block min-w-[150px] max-w-[150px] text-left"
     >
       <div>
-        <Menu.Button className="inline-flex w-full bg-brand-blue justify-between gap-x-1.5 whitespace-nowrap rounded-xl px-3 py-2 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        <Menu.Button className="inline-flex w-full justify-between gap-x-1.5 whitespace-nowrap rounded-xl bg-brand-blue px-3 py-2 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
           {`${name} ${selected}`}
           <ChevronDownIcon
             className="mr-1 h-5 w-5 text-gray-400"
@@ -99,20 +77,15 @@ const DropDownButton: React.FC<DropDownButtonProps<SortEvents | SortDateRange>> 
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items className="absolute left-0 z-10 mt-2 min-w-[150px] max-w-[150px] origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">{
-            children
-          // An attempt to pass useSelectedAtom to the children.
-          // React.Children.map(children, (child) => {
-          //     if (React.isValidElement(child)) {
-          //       return React.cloneElement(child, { useSelectedAtom });
-          //     }
-          //     return child;
-          //   })
-            }</div>
+          <div className="py-1">
+            {React.Children.map(children, child =>
+              React.cloneElement(child as React.ReactElement, { setSelected })
+            )}
+          </div>
         </Menu.Items>
       </Transition>
     </Menu>
   );
-}
+};
 
-export {DropDownButton, DropDownItem};
+export { DropDownButton, DropDownItem };
