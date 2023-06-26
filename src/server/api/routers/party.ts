22c6b5657cadc3ec480b4e736ff30771c5e7e4b8
meow <meow@taiwan>
2023-06-26 15:45:52 -0700

    update

--- src/server/api/routers/party.ts
+++ src/server/api/routers/party.ts
@@ -1,146 +1,190 @@
-import { contextProps } from "@trpc/react-query/shared";
-import { number, z } from "zod";
-import {
-  createTRPCRouter,
-  publicProcedure,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server"
+import { z } from "zod"
+import { createTRPCRouter, publicProcedure, protectedProcedure } from "~/server/api/trpc"
 
-const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"];
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png"]
 
 export const partyRouter = createTRPCRouter({
+   getParty: protectedProcedure
+      .input(
+         z.object({
+            id: z.number(),
+         })
+      )
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.party.findUnique({
+            where: {
+               id: input.id,
+            },
+         })
 
-    getParty : protectedProcedure
-      .input(z.object({
-        id : z.number()
-      }))
-      .query(async ({ ctx,input }) => {
-        const result = await ctx.prisma.parties.findUnique({
-          where: {
-            id: input.id,
-          }
-        })
-
-        return {
-          party : result
-        }
+         return {
+            party: result,
+         }
       }),
 
-  makeParty: protectedProcedure
-    .input(z.object({ 
-        name: z.string(), 
-        host: z.number(), 
-        hostid: z.number(), 
-        chatid: z.string(),
-        bannerimage: z.any()
-            .refine(
-                (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-                "Error: only .jpeg, .jpg, and .png types are accepted"
-            )
-    }))
-    .query(async ({ ctx,input }) => {
-        const result = await ctx.prisma.parties.create({
-          data: {
-            name : input.name,
-            host : input.host,
-            hostid : input.hostid,
-            chatid : input.chatid,
-            bannerimage : input.bannerimage
-          }
-        })
-
-        return {
-          party : result
-        }
+   getPartiesByHost: protectedProcedure
+      .input(z.number())
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.party
+            .findMany({ where: { host_id: input, } })
+            .catch(message => {
+               throw new TRPCError({
+                  code: "INTERNAL_SERVER_ERROR",
+                  message,
+               })
+            })
+      }),
+   getPartiesByGuest: protectedProcedure
+      .input(z.number())
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.attendance
+            .findMany({ where: { guest: input, } })
+            .catch(message => {
+               throw new TRPCError({
+                  code: "INTERNAL_SERVER_ERROR",
+                  message,
+               })
+            })
+      }),
+   getPartiesByCity: protectedProcedure
+      .input(z.string())
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.party
+            .findMany({ where: { city: input, } })
+            .catch(message => {
+               throw new TRPCError({
+                  code: "INTERNAL_SERVER_ERROR",
+                  message,
+               })
+            })
       }),
 
-  editPartyName : protectedProcedure
-      .input(z.object({
-        id : z.number(),
-        name: z.string()
-      }))
-      .query(async ({ ctx,input }) => {
-        const result = await ctx.prisma.parties.update({
-          where: {
-            id: input.id,
-          },
-          data: {
-            name: input.name,
-          },
-        })
+   makeParty: protectedProcedure
+      .input(
+         z.object({
+            name: z.string(),
+            host: z.number(),
+            hostid: z.number(),
+            chatid: z.string(),
+            bannerimage: z
+               .any()
+               .refine(
+                  (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+                  "Error: only .jpeg, .jpg, and .png types are accepted"
+               ),
+         })
+      )
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.parties.create({
+            data: {
+               name: input.name,
+               host: input.host,
+               hostid: input.hostid,
+               chatid: input.chatid,
+               bannerimage: input.bannerimage,
+            },
+         })
 
-        return {
-          party : result
-        }
+         return {
+            party: result,
+         }
       }),
 
-  editPartyHost : protectedProcedure
-      .input(z.object({
-        id : z.number(),
-        host : z.number(),
-        hostid : z.number(),
-        chatid : z.string()
-      }))
-      .query(async ({ ctx,input }) => {
-        const result = await ctx.prisma.parties.update({
-          where: {
-            id: input.id,
-          },
-          data: {
-            host: input.host,
-            hostid: input.hostid,
-            chatid: input.chatid
-          },
-        })
+   editPartyName: protectedProcedure
+      .input(
+         z.object({
+            id: z.number(),
+            name: z.string(),
+         })
+      )
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.parties.update({
+            where: {
+               id: input.id,
+            },
+            data: {
+               name: input.name,
+            },
+         })
 
-        return {
-          party : result
-        }
+         return {
+            party: result,
+         }
       }),
 
-  editPartyImage : protectedProcedure
-      .input(z.object({
-        id : z.number(),
-        bannerimage: z.any()
-        .refine(
-            (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
-            "Error: only .jpeg, .jpg, and .png types are accepted"
-        )
-      }))
-      .query(async ({ ctx,input }) => {
-        const result = await ctx.prisma.parties.update({
-          where: {
-            id: input.id,
-          },
-          data: {
-            bannerimage: input.bannerimage,
-          },
-        })
+   editPartyHost: protectedProcedure
+      .input(
+         z.object({
+            id: z.number(),
+            host: z.number(),
+            hostid: z.number(),
+            chatid: z.string(),
+         })
+      )
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.parties.update({
+            where: {
+               id: input.id,
+            },
+            data: {
+               host: input.host,
+               hostid: input.hostid,
+               chatid: input.chatid,
+            },
+         })
 
-        return {
-          party : result
-        }
+         return {
+            party: result,
+         }
       }),
 
-  deleteParty : protectedProcedure
-      .input(z.object({
-        id : z.number()
-      }))
-      .query(async ({ ctx,input }) => {
-        const result = await ctx.prisma.parties.delete({
-          where: {
-            id: input.id,
-          }
-        })
+   editPartyImage: protectedProcedure
+      .input(
+         z.object({
+            id: z.number(),
+            bannerimage: z
+               .any()
+               .refine(
+                  (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+                  "Error: only .jpeg, .jpg, and .png types are accepted"
+               ),
+         })
+      )
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.parties.update({
+            where: {
+               id: input.id,
+            },
+            data: {
+               bannerimage: input.bannerimage,
+            },
+         })
 
-        return {
-          result : result
-        }
+         return {
+            party: result,
+         }
       }),
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.parties.findMany();
-  }),
+   deleteParty: protectedProcedure
+      .input(
+         z.object({
+            id: z.number(),
+         })
+      )
+      .query(async ({ ctx, input }) => {
+         const result = await ctx.prisma.parties.delete({
+            where: {
+               id: input.id,
+            },
+         })
 
+         return {
+            result: result,
+         }
+      }),
 
-});
+   getAll: publicProcedure.query(({ ctx }) => {
+      return ctx.prisma.parties.findMany()
+   }),
+})
