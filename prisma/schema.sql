8be56e7a18ab61d94e047bda61fdd01d274f6962
meow <meow@taiwan>
2023-07-07 18:18:30 -0700

    update

--- prisma/schema.sql
+++ prisma/schema.sql
@@ -67,19 +67,19 @@ CREATE TABLE Session (
 -- is_private means unlisted & location is hidden until rsvp
 -- is_deleted completely hides the party, but saved for bill record reasons
 CREATE TABLE Party (
-   id             SERIAL,
-   party_name     VARCHAR(255)   NOT NULL,
-   banner_image   Link,
-   chat_id        VARCHAR(255),
-   created        TIMESTAMP      DEFAULT CURRENT_TIMESTAMP NOT NULL,
-   time_start     TIMESTAMP      NOT NULL,
-   time_end       TIMESTAMP      DEFAULT NULL,
-   is_waitlist    BOOLEAN        DEFAULT FALSE NOT NULL,
-   party_size     INT            DEFAULT NULL,  -- null = infinite
-   price          INT            DEFAULT 0 NOT NULL,
-   is_private     BOOLEAN        DEFAULT FALSE NOT NULL,
-   is_deleted     BOOLEAN        DEFAULT FALSE NOT NULL,
-   widgets        JSONB,
+   id            SERIAL,
+   party_name    VARCHAR(255)   NOT NULL,
+   banner        Link,
+   chat_id       VARCHAR(255),
+   created       TIMESTAMP      DEFAULT CURRENT_TIMESTAMP NOT NULL,
+   time_start    TIMESTAMP      NOT NULL,
+   time_end      TIMESTAMP      DEFAULT NULL,
+   is_waitlist   BOOLEAN        DEFAULT FALSE NOT NULL,
+   party_size    INT            DEFAULT NULL,  -- null = infinite
+   price         INT            DEFAULT 0 NOT NULL,
+   is_private    BOOLEAN        DEFAULT FALSE NOT NULL,
+   is_deleted    BOOLEAN        DEFAULT FALSE NOT NULL,
+   widgets       JSONB,
 
    state          VARCHAR(255)   NOT NULL,
    city           VARCHAR(255)   NOT NULL,
