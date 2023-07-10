7336d54c526a313c5aa83469aec7b305b0159cd8
meow <meow@taiwan>
2023-07-10 16:25:50 -0700

    vital fix to login.ts

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -1,8 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 
 export default function handler(req: NextRequest, res: NextResponse) {
-   if (req.method !== "POST") 
-   
    res.cookies.set("token", "token")
    res.cookies.set("account", "1")
    return {
