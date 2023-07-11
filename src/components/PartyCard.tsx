6334338e2ac45a5c0c2f1a4fdbe30a1e43fa3c72
meow <meow@taiwan>
2023-07-10 17:11:26 -0700

    fixed component errors

--- src/components/PartyCard.tsx
+++ src/components/PartyCard.tsx
@@ -1,32 +1,30 @@
-'use client';
+"use client"
 
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { parties } from '@prisma/client'
+import React from "react"
+import { party } from "@prisma/client"
 
 type PartyCardProps = {
-    party: parties
+   party: party
 }
 
-const PartyCard: React.FC<PartyCardProps> = ({party}) => {
-    return (
-        <div className='party_card'>
-            <div className='flex justify-between items-start gap-5'>
-                <img
-                    src={`data:image/png;base64,${party.bannerimage.toString('base64')}`}
-                    alt="party_banner"
-                    width={40}
-                    height={40}
-                    className="rounded-full object-contain"
-                />
-
-            </div>
-            <div className='flex flex-col'>
-                <h3>{party.host}</h3>
-                <p>{party.name}</p>
-            </div>
-        </div>
-    )
+const PartyCard: React.FC<PartyCardProps> = ({ party }) => {
+   return (
+      <div className="party_card">
+         <div className="flex items-start justify-between gap-5">
+            <img
+               src={`data:image/png;base64,${party.banner?.toString()}`}
+               alt="party_banner"
+               width={40}
+               height={40}
+               className="rounded-full object-contain"
+            />
+         </div>
+         <div className="flex flex-col">
+            <h3>{party.host}</h3>
+            <p>{party.party_name}</p>
+         </div>
+      </div>
+   )
 }
 
 export default PartyCard
