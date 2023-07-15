51f62c0b7ae4dd385d9078127c28d267918ad09c
meow <meow@taiwan>
2023-07-15 02:44:04 -0700

    update

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -1,4 +1,5 @@
 import { TRPCError } from "@trpc/server"
+import { z } from "zod"
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc"
 
 export const authRouter = createTRPCRouter({
@@ -6,8 +7,8 @@ export const authRouter = createTRPCRouter({
       .query(async ({ ctx }) => {
          const result = await ctx.prisma.session
             .findUnique({
-               where: { 
-                  token: ctx.session.token, 
+               where: {
+                  token: ctx.session.token,
                },
             })
             .catch(message => {
@@ -23,5 +24,31 @@ export const authRouter = createTRPCRouter({
       .query(async ({ ctx }) => {
          ctx.res.setHeader("Set-Cookie", "token=hello;")
          return { message: "Cookie set" }
-      })
+      }),
+   authorizeUser: publicProcedure
+      .input(z.object({
+         email: z.string(),
+         password: z.string(),
+      }))
+      .query(async ({ ctx, input }) => {
+
+      }),
+   registerUser: publicProcedure
+      .input(z.object({
+         email: z.string(),
+         password: z.string(),
+      }))
+      .query(async ({ ctx, input }) => {
+         const is_user_unique = await ctx.prisma.account
+            .findFirst({ where: { email: input.email } })
+            .catch(message => {
+               throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message })
+            })
+         if (is_user_unique) {
+            throw new TRPCError({
+               code: "CONFLICT",
+               message: "User already exists",
+            })
+         }
+      }),
 })
