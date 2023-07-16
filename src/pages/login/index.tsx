97699bcb47ba74f74c277ea4b873b345a2b1251c
meow <meow@>
2023-07-16 02:47:02 -0700

    update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react"
+import { api } from "~/utils/api"
+
 export default function Login() {
-   return <section>
-      <h1>Login</h1>
-      <input type="text" placeholder="email" />
-      <input type="password" placeholder="password" />
-      <button>Submit</button>
-   </section>
+   const [email, setEmail] = useState("")
+   const [pass, setPass] = useState("")
+
+   async function onSubmit() {
+      const client = api.useContext()
+   }
+
+   useEffect(() =>{
+      
+   })
+
+   return (
+      <section>
+         <h1>Login</h1>
+         <input
+            type="text"
+            placeholder="email"
+            onChange={e => setEmail(e.target.value)}
+            value={email}
+         />
+         <input
+            type="password"
+            placeholder="password"
+            onChange={e => setPass(e.target.value)}
+            value={pass}
+         />
+         <button onClick={onSubmit}>Submit</button>
+      </section>
+   )
 }
