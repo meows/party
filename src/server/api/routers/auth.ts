96cb3536f1dbac4d4b0580c426795b2654ed6cd1
meow <meow@>
2023-07-17 08:28:41 -0700

    update

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -33,7 +33,6 @@ export const authRouter = createTRPCRouter({
    loginOrRegister: publicProcedure
       .input(z.object({ email: z.string(), password: z.string() }))
       .mutation(async ({ ctx, input: { email, password } }) => {
-         // retrieve existing user || create new user
          const hash = await ctx.bcrypt.hash(password, 10)
          const now = new Date()
          const result = await ctx.prisma.account
@@ -58,7 +57,6 @@ export const authRouter = createTRPCRouter({
 
          if (result.created.getTime() === now.getTime()) console.log("New account: ", result)
 
-         // generate session
          const token = randomString(32)
          const session = await ctx.prisma.session
             .create({
