15d37c35ee02de7d813b4b660612cdde13f8aa99
meow <meow@>
2023-07-17 07:55:02 -0700

    update

--- src/server/api/routers/auth.ts
+++ src/server/api/routers/auth.ts
@@ -4,6 +4,7 @@ import { randomString } from "~/lib/utility"
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "~/server/api/trpc"
 
 export const authRouter = createTRPCRouter({
+   /** Check if you have good auth cookies. */
    isAuth: protectedProcedure
       .query(async ({ ctx }) => {
          const result = await ctx.prisma.session
@@ -19,37 +20,6 @@ export const authRouter = createTRPCRouter({
                })
             })
          return result
-      }),
-   giveCookie: publicProcedure
-      .query(async ({ ctx }) => {
-         ctx.res.setHeader("Set-Cookie", "token=hello;")
-         return { message: "Cookie set" }
-      }),
-   authorizeUser: publicProcedure
-      .input(z.object({
-         email: z.string(),
-         password: z.string(),
-      }))
-      .query(async ({ ctx, input }) => {
-
-      }),
-   registerUser: publicProcedure
-      .input(z.object({
-         email: z.string(),
-         password: z.string(),
-      }))
-      .query(async ({ ctx, input }) => {
-         const is_user_unique = await ctx.prisma.account
-            .findFirst({ where: { email: input.email } })
-            .catch(message => {
-               throw new TRPCError({ code: "INTERNAL_SERVER_ERROR", message })
-            })
-         if (is_user_unique) {
-            throw new TRPCError({
-               code: "CONFLICT",
-               message: "User already exists",
-            })
-         }
       }),
    /**
     * If no password is present:
